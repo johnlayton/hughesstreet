7f0e01989988a5683548e4bce4e5cae20add94d1
john layton <john.layton@touchcorp.com>
2018-06-03 14:08:10 +1000

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -x "`command -v curl`" ]; then
+  CMD="curl -#L"
+elif [ -x "`command -v wget`" ]; then
+  CMD="wget --no-check-certificate -O -"
+fi
+
+if [ -z "$CMD" ]; then
+  echo "No curl or wget available. Aborting."
+else
+  mkdir -p "$HOME/.dotfiles" && \
+  eval "$CMD https://github.com/johnlayton/hughesstreet/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
+  . "$HOME/.dotfiles/install.sh"
+fi
