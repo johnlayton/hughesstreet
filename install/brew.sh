b26f8cbd4a10f4863ea0efbdecd389e27cd012c2
john layton <john.layton@touchcorp.com>
2018-06-03 16:27:53 +1000

    update brew install script

--- install/brew.sh
+++ install/brew.sh
@@ -1,5 +1,5 @@
 if ! command -v brew >/dev/null; then
-  echo "Installing Homebrew..."
+  echo "Homebrew installing..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 else
   echo "Homebrew already installed. Skipping."
